01cd94be84c99ef8febafbfcfeede27738d72fde
joshu_los1995 <joshu_los1995@outlook.com>
2023-01-31 01:19:02 -0300

    Add kyu_8 files

--- kyu_8/greet.py
+++ kyu_8/greet.py
@@ -1,23 +1,3 @@
 
-def greet(language):
-    #your code here
-    idioms = {'english': 'Welcome',
-    'czech': 'Vitejte',
-    'danish': 'Velkomst',
-    'dutch': 'Welkom',
-    'estonian': 'Tere tulemast',
-    'finnish': 'Tervetuloa',
-    'flemish': 'Welgekomen',
-    'french': 'Bienvenue',
-    'german': 'Willkommen',
-    'irish': 'Failte',
-    'italian': 'Benvenuto',
-    'latvian': 'Gaidits',
-    'lithuanian': 'Laukiamas',
-    'polish': 'Witamy',
-    'spanish': 'Bienvenido',
-    'swedish': 'Valkommen',
-    'welsh': 'Croeso'}
-    if language not in idioms:
-        return "Welcome"
-    return idioms[language]
+def greet():
+    return 'hello world!'
