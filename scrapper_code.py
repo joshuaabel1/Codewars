75ddfc4fc92fc57a973fc9cf15878261e4f6c8e3
joshuaabel1 <88058984+joshuaabel1@users.noreply.github.com>
2023-02-01 01:59:06 -0300

    Update scrapper_code.py

--- scrapper_code.py
+++ scrapper_code.py
@@ -85,18 +85,17 @@ def build_functions(func_list):
                 f.write(f"[submodule \"kyu_{kyu}\"]\n\tpath = kyu_{kyu}\n\turl = https://github.com/joshuaabel1/Codewars/tree/main/kyu_{kyu}\n")
             repo.git.add(os.path.join(folder_name, file_name))
             repo.index.commit(f"Update {file_name}")
-        elif:
+        else:
             # ...
             submodule_url = f"https://github.com/joshuaabel1/Codewars/tree/main/kyu_{kyu}"
             if not submodule_url:
                 raise ValueError("A URL was not given and a repository did not exist at %s" % submodule_url)
             repo.create_submodule(f"kyu_{kyu}", submodule_url)
-        else:
-            repo.git.add(A=True)
-            repo.index.commit(f"Add kyu_{kyu} files")
             
         kyu_out = func[0]
     # Remove remote "origin" if it exists
+    repo.git.add(A=True)
+    repo.index.commit(f"Add kyu_{kyu} files")
     try:
         repo.remote("origin").remove(repo, "origin")
     except git.exc.GitCommandError as e:
