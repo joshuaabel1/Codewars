935240f2a25b35c78aa22a4f7ed81d19b10e18f8
joshuaabel1 <88058984+joshuaabel1@users.noreply.github.com>
2023-01-31 21:27:29 -0300

    Update scrapper_code.py

--- scrapper_code.py
+++ scrapper_code.py
@@ -80,11 +80,11 @@ def build_functions(func_list):
         if os.path.exists(os.path.join(folder_name, file_name)):
             repo.git.add(os.path.join(folder_name, file_name))
             repo.index.commit(f"Update {file_name}")
-        else:
-            submodule = repo.create_submodule("kyu_" + kyu, folder_name)
-            submodule.update(init=True)
-            repo.git.add("kyu_" + kyu)
-            repo.index.commit(f"Add kyu_{kyu} files as submodule")
+#         else:
+#             submodule = repo.create_submodule("kyu_" + kyu, folder_name)
+#             submodule.update(init=True)
+#             repo.git.add("kyu_" + kyu)
+#             repo.index.commit(f"Add kyu_{kyu} files as submodule")
     repo.remote("origin").remove(repo, "origin")
     origin = repo.create_remote(name='origin', url='https://github.com/joshuaabel1/Codewars.git')
     origin.push("main", set_upstream=True)
