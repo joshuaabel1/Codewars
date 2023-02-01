ec6b908e39efd4fa3df82bd537e85295c5292fab
joshuaabel1 <88058984+joshuaabel1@users.noreply.github.com>
2023-01-31 22:07:29 -0300

    Update scrapper_code.py

--- scrapper_code.py
+++ scrapper_code.py
@@ -87,8 +87,7 @@ def build_functions(func_list):
             repo.index.commit(f"Add kyu_{kyu} files")
     repo.remote("origin").remove(repo, "origin")
     origin = repo.create_remote(name='origin', url='https://github.com/joshuaabel1/Codewars.git')
-    progress = RemoteProgress()
-    origin.push("origin", "main", progress)
+    origin.push("origin", "main", progress=git.RemoteProgress())
     return functions
 
 
