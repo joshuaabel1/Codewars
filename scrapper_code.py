6c696417619c1b145fe8d0a514c13732537fe280
joshu_los1995 <joshu_los1995@outlook.com>
2023-02-05 23:42:44 -0300

    save changes and fixes

--- scrapper_code.py
+++ scrapper_code.py
@@ -5,11 +5,11 @@ from selenium.webdriver.common.by import By
 from git import Repo
 from dotenv import load_dotenv
 import time
-from selenium.webdriver import Firefox
+from selenium.webdriver import Firefox, Chrome
 
 load_dotenv()
 
-driver = Firefox()
+driver = webdriver.c()
 
 driver.get("https://www.codewars.com/users/sign_in")
 
@@ -77,7 +77,7 @@ def build_functions(func_list):
         else:
             repo.git.add(A=True)
             repo.index.commit(f"Add kyu_{kyu} files")
-    origin = repo.remote(name='origin')
+    origin = repo.remote(name='origin', url='https://github.com/joshuaabel1/Codewars.git')
     origin.push()
     return functions
 
