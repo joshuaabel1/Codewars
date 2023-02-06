0419eabc106b87d8896aa7af888e3427e8dc0c14
joshu_los1995 <joshu_los1995@outlook.com>
2023-02-06 00:26:42 -0300

    Add kyu_6 files

---
+++ kyu_6/find_even_index.py
@@ -0,0 +1,9 @@
+
+def find_even_index(arr):
+    #your code here
+    for i in range(len(arr)):
+        left_sum = sum(arr[:i])
+        right_sum = sum(arr[i+1:])
+        if left_sum == right_sum:
+            return i
+    return -1
